5b9997980e8829ac071e66e8952f61d19167ca2e
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-05-07 12:15:45 -0700

    added id attributes to the jsx elements

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -9,12 +9,12 @@ test("renders ContactForm without crashing", () => {
 
 //test that the forms are there
 test('forms are present', () => {
-     const { getByText } = render(<ContactForm />)
+     const { getByText, getByLabelText } = render(<ContactForm />)
 
-     const firstNameInput = getByText(/first name*/i)
-     const lastNameInput = getByText(/last name*/i)
-     const emailInput = getByText(/email*/i)
-     const messageInput = getByText(/message/i)
+     const firstNameInput = getByLabelText(/first name*/i)
+     const lastNameInput = getByLabelText(/last name*/i)
+     const emailInput = getByLabelText(/email*/i)
+     const messageInput = getByLabelText(/message/i)
 
      expect(firstNameInput).toBeInTheDocument()
      expect(lastNameInput).toBeInTheDocument()
@@ -24,3 +24,13 @@ test('forms are present', () => {
 })
 
 //test that the forms can submit
+// test('forms are present', () => {
+//      const { getByText } = render(<ContactForm />)
+
+//      const firstNameInput = getByText(/first name*/i)
+//      const lastNameInput = getByText(/last name*/i)
+//      const emailInput = getByText(/email*/i)
+//      const messageInput = getByText(/message/i)
+//      const submit = getByText()
+
+// })
